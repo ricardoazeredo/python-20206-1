bac7fc91916c008faa05e9907dad4f50cdae5f0d
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2026-06-23 20:26:36 -0300

    update: add modularização

---
+++ aula-5/index.py
@@ -0,0 +1,8 @@
+
+from meu_modulo import somar, multiplicacao, saudacao
+
+resultado = multiplicacao(4,5)
+print("Multiplicação:", resultado) 
+print(saudacao("Carlos")) 
+
+print("a soma é: ",somar(7,4))
---
+++ aula-5/meu_modulo.py
@@ -0,0 +1,9 @@
+
+def multiplicacao(a, b): 
+    return a * b 
+
+def saudacao(nome): 
+    return f"Olá, {nome}!" 
+
+def somar(num1,num2):
+    return num1 + num2
